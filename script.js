10f50549fc2afd2a1043cba4341db973a498204e
muhammadanassaleem <anassaleem555590@gmail.com>
2026-07-30 20:40:54 +0500

    api fetching done

--- script.js
+++ script.js
@@ -12,20 +12,39 @@ toggleButtons.forEach((btn) => {
 let searchBar = document.querySelector(".search-field");
 let form = document.querySelector(".search-bar");
 
-const getWeather = async (event) => {
-  event.preventDefault();
+//getting response
+const getCordinates = async (city) => {
   const response = await fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=24.85&longitude=66.99&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,weathercode,uv_index&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min,weathercode&hourly=temperature_2m,weathercode&timezone=Asia/Karachi&forecast_days=2",
+    `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`,
   );
   const data = await response.json();
-  console.log(data);
+  let cityName = data.results[0].name;
+  let country = data.results[0].country;
+  console.log(cityName, country);
 
-  document.querySelector(".weather-temp").textContent =
-    data.current.temperature_2m + "°C";
-  document.querySelector(".feels-like").textContent =
-    "Feels like " + data.current.apparent_temperature + "°C";
+  console.log(data, "getcordinates");
+  return {
+    lat: data.results[0].latitude,
+    lon: data.results[0].longitude,
+  };
+};
+const getWeather = async (event) => {
+  event.preventDefault();
+  let city = searchBar.value;
+  const cordinates = await getCordinates(city);
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${cordinates.lat}&longitude=${cordinates.lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,weathercode,uv_index&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min,weathercode&hourly=temperature_2m,weathercode&timezone=Asia/Karachi&forecast_days=2`,
+  );
+  const data = await response.json();
+  console.log(data, "getweather");
+  // getting response end here
 
-  console.log(searchBar.value);
+  // document.querySelector(".weather-temp").textContent =
+  //   data.current.temperature_2m + "°C";
+  // document.querySelector(".feels-like").textContent =
+  //   "Feels like " + data.current.apparent_temperature + "°C";
+
+  // console.log(data.current.temperature_2m);
 };
 
 form.addEventListener("submit", getWeather);
